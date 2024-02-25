b015cf469d3ceac138e14e33e850ca323cfcb81d
sendmailtothedevil <sendmailtothedevil@gmail.com>
2024-02-25 23:40:45 +0100

    moving

--- invest/views.py
+++ invest/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.core.checks import messages
+from django.shortcuts import redirect, render
+from .models import *
+from django.http import JsonResponse
+
 
 # Create your views here.
+def invest(request):
+    
+    context = {}
+    return render(request, 'invest/invest.html', context)
+
